d3ddcbdc04cc52933b5163fadbcbba2bddb51acd
zoolye <www.iphone@foxmail.com>
2018-11-13 13:47:58 +0800

    feat:新增斗鱼视屏

--- src/main/java/zool/firefly/controller/HelloController.java
+++ src/main/java/zool/firefly/controller/HelloController.java
@@ -1,15 +1,14 @@
 package zool.firefly.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : zoolye
  * @date : 2018-11-03 11:17
  * @describe :
  */
-@RestController
+@Controller
 public class HelloController {
 
     //@Value("${my.name}")
@@ -20,12 +19,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
-    @RequestMapping(value = "/personInfo")
-    public String getPersonInfo(){
-        return name+" : "+age;
+        return "nus";
     }
 
 }
